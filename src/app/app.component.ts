3c9071e45da96aa35ee8bd602ef51a1259389b99
patrickaregan <patrickaregan@gmail.com>
2022-09-03 23:57:03 -0500

    update styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', './style-custom.css', './style-google.css', './style.css']
 })
 export class AppComponent {
   title = 'portfolio';
